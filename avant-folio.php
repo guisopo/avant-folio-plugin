4fda2e891a141dad7b738c270ee11c10e3d883ad
guisopo <solerguillermo@hotmail.com>
2019-08-26 20:36:38 +0200

    Move plugin to a class and follow OOP practices

--- avant-folio.php
+++ avant-folio.php
@@ -8,66 +8,33 @@ Author URI: http://github.com/guisopo
 License: GPLv2
 */
 
+// If something else external from the website is accesing those files ABSPATH is not defined
+defined ( 'ABSPATH' ) or die('You cannot acces this file!');
+
+// If this file is called directly, abort
+if ( ! defined( 'WPINC' ) ){
+  die;
+}
+
+/**
+ * Include core class for executing the plugin
+ */
+
+require_once( plugin_dir_path(__FILE__) . 'includes/admin/class-avant-folio.php' );
+
+/**
+ * Begin excution of plugin
+ */
+function run_avant_folio() {
+  $plugin = new Avant_Folio();
+  $plugin->run();
+}
+run_avant_folio();
+
+function avant_folio_activation() {
+  // flush rewrite rules: tells WP something is happenning in the DB and needs to refresh in order to read the new information
+  flush_rewrite_rules();
+}
+
 // Call function when plugin is activated
-register_activation_hook( __FILE__, 'avant_folio_install' );
-
-function avant_folio_install() {
-}
-
-add_action( 'admin_menu', 'avant_folio_create_menu' );
-
-function avant_folio_create_menu() {
-  add_menu_page(
-    'Portfolio Plugin Page', 
-    'Portfolio', 
-    'manage_options', 
-    'avant_folio', 
-    'avant_folio_profile_page', 
-    'dashicons-admin-customizer',
-    '2'
-  );
-
-  add_submenu_page(
-    'avant_folio',
-    'User Profile Page',
-    'User Profile',
-    'manage_options',
-    'avant_folio',
-    'avant_folio_profile_page'
-  );
-
-  add_submenu_page(
-    'avant_folio',
-    'Portfolio Settings Page',
-    'Portfolio Settings',
-    'manage_options',
-    'avant_folio_settings', 
-    'avant_folio_settings_page'
-  );
-
-  add_action( 'admin_init', 'avant_folio_register_settings' );
-}
-
-function avant_folio_register_settings() {
-  register_setting( 'avant-folio-settings-group', 'avant_folio_options', 'avant_folio_sanitize_options' );
-}
-
-function avant_folio_sanitize_options( $input ) {
-  $input['option_name'] = sanitize_text_field( $input['option_name'] );
-  $input['option_last_name'] = sanitize_text_field( $input['option_last_name'] );
-  $input['option_twitter'] = avant_folio_sanitize_at_symbol( $input['option_twitter'] );
-  $input['option_instagram'] = avant_folio_sanitize_at_symbol( $input['option_instagram'] );
-  $input['option_facebook'] = sanitize_text_field( $input['option_facebook'] );
-  
-  return $input;
-}
-
-function avant_folio_profile_page() {
-  require_once( plugin_dir_path( __FILE__ )  . '/includes/templates/avant-folio-admin.php' );
-}
-
-function avant_folio_sanitize_at_symbol( $input ){
-	$output = sanitize_text_field( $input );
-	$output = str_replace('@', '', $output);
-	return $output;
-}
+register_activation_hook( __FILE__, 'run_avant_folio' );
---
+++ includes/admin/class-avant-folio.php
@@ -0,0 +1,100 @@
+<?php
+
+class Avant_Folio {
+
+  private $name;
+  private $version;
+
+  public function __construct() {
+
+    $this->name = 'avant_folio';
+    $this->version = '0.1.0';
+  }
+
+  public function run() {
+    // add_action( 'admin_init', array( $this, 'register_settings' ) );
+    add_action( 'admin_menu', array( $this, 'addMenuPage' ) );
+  }
+
+  public function addMenuPage() {
+    add_menu_page(
+      $this->name,
+      'Portfolio',
+      'manage_options',
+      $this->name,
+      array( $this, 'profile_page' ),
+      'dashicons-admin-customizer',
+      '2'
+    );
+
+    $this->addSubMenuPages();
+  }
+
+  public function addSubMenuPages() {
+
+    $subMenuPages = array(
+      // User Profiles
+      array(
+        'parent_slug' => $this->name,
+        'page_title'  => 'User Profile Page',
+        'menu_title'  => 'User Pofile',
+        'capability'  => 'manage_options',
+        'menu_slug'   => $this->name,
+        'callback'    => array( $this, 'profile_page' ) 
+      ),
+      // Portfolio Settings
+      array(
+        'parent_slug' => $this->name,
+        'page_title'  => 'Portfolio Settings Page',
+        'menu_title'  => 'Portfolio Settings',
+        'capability'  => 'manage_options',
+        'menu_slug'   => 'avant_folio_settings',
+        'callback'    => array( $this, 'settings_page' )
+      )
+    );
+
+    foreach ( $subMenuPages as $subpage) {
+      add_submenu_page(
+        $subpage['parent_slug'],
+        $subpage['page_title'],
+        $subpage['menu_title'],
+        $subpage['capability'],
+        $subpage['menu_slug'],
+        $subpage['callback']
+      );
+    }
+  }
+
+  public function register_settings() {
+
+    register_setting( 
+      'avant-folio-settings-group', 
+      'avant_folio_options', 
+      $this-> array($this, 'sanitize_settings')
+    );
+  }
+
+  public function profile_page() {
+    require_once( plugin_dir_path( __FILE__ )  . '../templates/avant-folio-admin.php' );
+  }
+
+  public function settings_page() {
+    require_once( plugin_dir_path( __FILE__ )  . '../templates/avant-folio-settings.php' );
+  }
+
+  public function sanitize_settings( $input ) {
+    $input['option_name'] = sanitize_text_field( $input['option_name'] );
+    $input['option_last_name'] = sanitize_text_field( $input['option_last_name'] );
+    $input['option_twitter'] = $this-> array($this, 'sanitize_at_symbol' );
+    $input['option_instagram'] = $this-> array($this, 'sanitize_at_symbol' );
+    $input['option_facebook'] = sanitize_text_field( $input['option_facebook'] );
+    
+    return $input;
+  }
+
+  public function sanitize_at_symbol( $input ){
+    $output = sanitize_text_field( $input );
+    $output = str_replace('@', '', $output);
+    return $output;
+  }
+}
