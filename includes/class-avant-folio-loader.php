a42a64e3c62f979ed3d9abd444097c5082ff4bf7
guisopo <solerguillermo@hotmail.com>
2019-08-27 16:40:14 +0200

    Complete class

--- includes/class-avant-folio-loader.php
+++ includes/class-avant-folio-loader.php
@@ -1,26 +1,39 @@
 <?php
-class AvantFolioLoader {
+class Avant_Folio_Loader {
   
   protected $actions;
   protected $filters;
 
   public function __construct() {
-
+    $this->actions = $actions;
+    $this->filters = $filters;
   }
 
   public function add_action( $hook, $component, $callback) {
-
+    $this->actions = $this->add( $this->actions, $hook, $component, $callback);
   }
 
   public function add_filter( $filter, $component, $callback ) {
-
+    $this->filters = $this->add( $this->filters, $filter, $component, $callback);
   }
   
   private function add( $hooks, $hook, $component, $callback) {
-    
+    $hooks[] = array(
+      'hook' => $hook,
+      'component' => $component,
+      'callback' => $callback
+    );
+
+    return $hooks;
   }
 
   public function run() {
-    
+    foreach ( $this->filters as $hook ) {
+      add_filter( $hook['hook'], array( $hook['component'], $hook['callback'] ) );
+    }
+
+    foreach ( $this->actions as $hook ) {
+      add_action( $hook['hook'], array( $hook['component'], $hook['callback'] ) );
+    }
   }
 }
