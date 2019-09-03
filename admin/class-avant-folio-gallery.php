1d95f22ce2168e31c2f0b43b9798e7800c29e9a2
guisopo <solerguillermo@hotmail.com>
2019-09-03 19:48:52 +0200

    fix small bugs

--- admin/class-avant-folio-gallery.php
+++ admin/class-avant-folio-gallery.php
@@ -5,19 +5,53 @@ class Avant_Folio_Gallery {
   protected $name;
   protected $version;
 
-  public function __construct() {
+  protected $id;
+  protected $title;
+  protected $context;
+  protected $priority;
 
-    $this->name    = 'Image Gallery';
+  public function __construct( $id = 'Work Gallery', $title = '', $context = 'advanced', $priority = 'high' ) {
+    
+    $this->id = $id;
+
+    if( $title == '' ) {
+      $this->title = ucfirst($this->id);
+    } else {
+      $this->title = $title;
+    } 
+
+    $available_context = array(
+      'advanced',
+      'side',
+      'normal'
+    );
+
+    if( in_array( $context, $available_context ) ) {
+      $this->context = $context;
+    }
+
+    $available_priority = array(
+      'default',
+      'high',
+      'normal'
+    );
+
+    if( in_array( $priority, $available_priority ) ) {
+      $this->priority = $priority;
+    }
+
     $this->version = '0.1.0';
   }
 
   public function add_metabox() { 
-
+    
     add_meta_box(
-      $this->name,
-      'Work Images',
+      $this->id,
+      $this->title,
       array( $this, 'render' ),
-      'works'
+      'works',
+      $this->context,
+      $this->priority
     );
   }
 
@@ -27,7 +61,7 @@ class Avant_Folio_Gallery {
 
     
     wp_enqueue_script(
-      $this->name,
+      $this->id,
       plugin_dir_url( __FILE__ ) . 'js/avant-folio-gallery.js',
       array( 'jquery' ),
       $this->version,
@@ -35,8 +69,14 @@ class Avant_Folio_Gallery {
     );
   }
 
-  public function render() {
+  public function render( $post ) {
+    
+    wp_nonce_field( basename( __FILE__ ), $this->metabox_nonce );
 
     require_once plugin_dir_path( dirname( __FILE__ ) ) . 'partials/avant-folio-gallery.php';
   }
+
+  public function save_images() {
+
+  } 
 }
