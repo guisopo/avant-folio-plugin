42613d57c4bf692bb97a3da4506e62b8ab906413
guisopo <solerguillermo@hotmail.com>
2019-09-03 11:02:32 +0200

    Optimize code and create removeInput mehtod

--- admin/js/avant-folio-gallery.js
+++ admin/js/avant-folio-gallery.js
@@ -3,99 +3,135 @@ console.log('Avant-Folio-Gallery');
 class mediaUploader   {
 
   constructor() {
-    console.log('constructed');
 
     this.file_frame;
     this.image_data;
     this.json;
 
     this.addImagesButton = document.getElementById('js-add-images');
-    this.removeImagesButton = document.getElementById('js-remove-images');
     this.galleryContainer = document.getElementById('js-gallery-container');
-    this.selectedImages = [];
 
+    this.selectedImages = [];
+    this.inputValue = [];
+    this.galleryHiddenInput;
+
+    console.log('Constructed');
     this.init();
   }
 
   bindAll() {
-    console.log('bindAll');
-
     ['renderMediaUploader', 'removeImage']
       .forEach( fn => this[fn] = this[fn].bind(this));
+
+    console.log('Bind All');
   }
 
   renderMediaUploader(e) {
-    console.log('Click: Render Media Uploader!');
-    
     e.preventDefault();
 
     if ( this.file_frame !== undefined ) {
         this.file_frame.open();
         return;
     }
-
+    
     this.file_frame = wp.media.frames.file_frame = wp.media({
         frame:  'post',
         state:  'insert',
         multiple: true
     });
 
+    console.log('Click: Render Media Uploader');
+
     this.file_frame.on( 'insert', () => {
       this.json = this.file_frame.state().get('selection').toJSON();
-      
-      this.createImages();
+
+      this.json.forEach(imageData => {
+        if ( 0 > imageData.url.trim() ) {
+          return;
+        }
+        this.createImage(imageData);
+      });
       this.renderImages();
+      this.createInput();
+      this.setInputValue();
     });
 
     this.file_frame.open();
   }
 
-  createImages() {
-    
-    this.json.forEach( imageData => {
-      console.log('Image Created');
-      if ( 0 > imageData.url.trim() ) {
-        return;
-      }
-      // Create Image
-      const image = document.createElement('img');
-      image.classList.add('work-image');
-      image.setAttribute('src', imageData.url);
-      image.setAttribute('alt', imageData.caption);
-      image.setAttribute('title', imageData.title);
-      
-      image.addEventListener('click', this.removeImage);
+  createImage(imageData) {
+    // Create Image
+    const image = document.createElement('img');
+    image.classList.add('work-image');
+    image.setAttribute('src', imageData.url);
+    image.setAttribute('alt', imageData.caption);
+    image.setAttribute('title', imageData.title);
+    image.setAttribute('data-id', imageData.id);
 
-      this.galleryContainer.appendChild(image);
-      this.selectedImages.push(image);
-    });
+    this.selectedImages.push(image);
+    // Add Event-Listener to Image
+    image.addEventListener('click', this.removeImage);
+    // Append to Gallery Container
+    this.galleryContainer.appendChild(image);
+
+    console.log(`Image ${imageData.id} created`);
   }
 
   renderImages() {
-    console.log('Show Images!');
-
     this.galleryContainer.classList.remove('hidden');
+
+    console.log('Show Images!');
+  }
+
+  createInput() {
+    this.galleryHiddenInput = document.createElement('input');
+    this.galleryHiddenInput.setAttribute('type', 'hidden');
+    this.galleryHiddenInput.setAttribute('id', 'avant_folio_work_gallery');
+    this.galleryHiddenInput.setAttribute('name', 'avant_folio_work_info[gallery]');
+    this.galleryContainer.appendChild(this.galleryHiddenInput);
+
+    console.log('Hidden Meta Box Created');
+  }
+
+  removeInput() {
+    this.galleryHiddenInput.remove();
+
+    console.log(`Remove Input`)
+  }
+
+  setInputValue() {
+    this.inputValue = [];
+    this.selectedImages.forEach(image => this.inputValue.push(image.dataset.id));
+
+    this.galleryHiddenInput.setAttribute('value', this.inputValue);
+
+    console.log(`Set Input Value: ${this.galleryHiddenInput.value || null}`);
   }
 
   removeImage(e) {
-    console.log('Image removed');
-
-    e.preventDefault();
 
     e.target.classList.add('hidden');
-
+    e.target.remove();
     this.selectedImages = this.selectedImages.filter( image => image !== e.target);
+
+    console.log('Click: Image removed');
+
+    if( this.selectedImages.length > 0 ) {
+      this.setInputValue();
+    } else {
+      this.setInputValue();
+      this.removeInput();
+    }
   }
 
   addEvents() {
-    console.log('add events');
-
     this.addImagesButton.addEventListener('click', this.renderMediaUploader);
-    this.removeImagesButton.addEventListener('click', this.removeImage);
+    
+    console.log('Add events');
   }
 
   init() {
-    console.log('init');
+    console.log('Init');
 
     this.bindAll();
     this.addEvents();
