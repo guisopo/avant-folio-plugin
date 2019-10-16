fe19d96c4c061cf79efd48bdad540e5a5c97f5fd
guisopo <solerguillermo@hotmail.com>
2019-10-16 11:42:33 +0200

    Style featured image placeholder in gallery

--- partials/avant-folio-gallery.php
+++ partials/avant-folio-gallery.php
@@ -9,8 +9,11 @@ $images = isset( $meta_value['gallery'] ) ? explode(",", $meta_value['gallery'] 
 	<div class="af-gallery__featured">
 	
 		<h2 class="af-featured__title">Featured Image:</h2>
-
+		
 		<div class="af-featured__image">
+			<!-- <svg width="30" height="30" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true" focusable="false">
+				<path fill="none" d="M0 0h24v24H0V0z"></path><g><path d="M20 4v12H8V4h12m0-2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-8.5 9.67l1.69 2.26 2.48-3.1L19 15H9zM2 6v14c0 1.1.9 2 2 2h14v-2H4V6H2z"></path></g>
+			</svg> -->
 			<?php the_post_thumbnail( 'medium', [ 'id' => get_post_thumbnail_id() ] ); ?>
 		</div>
 
