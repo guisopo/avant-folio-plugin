50f353ea0a3eb92b60e827f3645cb51521489a28
guisopo <solerguillermo@hotmail.com>
2020-05-01 17:57:23 +0200

    Clean up

--- avant-folio.php
+++ avant-folio.php
@@ -1,16 +1,31 @@
 <?php
+/**
+ * @package AvantFolio
+ */
+
 /*
 Plugin Name: Avant Folio
 Description: Create a custom post type to handle a portfolio for artist
 Version: 1.0
 Author: Guillermo Soler Poquet
 Author URI: http://github.com/guisopo
-License: GPLv2
+License: GPLv2 or later
 */
 
-if( ! defined( 'ABSPATH' )) {
-  die;
-}
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 // If something else external from the website is accesing those files ABSPATH is not defined
 defined ( 'ABSPATH' ) or die('You cannot acces this file!');
