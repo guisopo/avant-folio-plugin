3cb3a00244750c00560259ab124eb78ddc34ccbe
guisopo <solerguillermo@hotmail.com>
2020-05-16 18:51:58 +0200

    Clean up

--- includes/Base/BaseController.php
+++ includes/Base/BaseController.php
@@ -14,8 +14,8 @@ class BaseController
   public $managers = array();
 
   public function __construct() {
-    $this->plugin_path = plugin_dir_path( dirname( __FILE__, 2 ) );
-    $this->plugin_url = plugin_dir_url( dirname( __FILE__, 2 ) );
-    $this->plugin = plugin_basename( dirname( __FILE__, 3 ) ) . '/avant-folio.php';
+    $this->plugin_path  = plugin_dir_path( dirname( __FILE__, 2 ) );
+    $this->plugin_url   = plugin_dir_url( dirname( __FILE__, 2 ) );
+    $this->plugin       = plugin_basename( dirname( __FILE__, 3 ) ) . '/avant-folio.php';
   }
 }
