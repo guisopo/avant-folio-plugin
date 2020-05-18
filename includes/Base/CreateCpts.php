c2879c966d96c15c0a94d5ca4f862d76fdfc8529
guisopo <solerguillermo@hotmail.com>
2020-05-18 18:56:04 +0200

    Capitalize cpt names

--- includes/Base/CreateCpts.php
+++ includes/Base/CreateCpts.php
@@ -22,25 +22,25 @@ class CreateCpts extends BaseController {
   {
     $this->cptData = array(
       array(
-        'cpt_name'     			=> 'works',
+        'cpt_name'     			=> 'Works',
         'cpt_supports' 			=> array( 'title', 'thumbnail', 'revisions', 'post-formats' ),
         'cpt_icon'     			=> 'dashicons-visibility',
         'cpt_custom_fields' => array(
           'id'       => 'work-information',
           'title'    =>	esc_html__( 'Work Details', 'string' ),
-          'screen'   => 'works',
+          'screen'   => 'Works',
           'meta-key' => 'avant_folio_work_info',
         ),
         'cpt_gallery'				=> array(
           'id'       => 'Work Gallery',
           'title'    =>	esc_html__( 'Work Gallery', 'string' ),
-          'screen'   => 'works',
+          'screen'   => 'Works',
           'context'	 => 'advanced',
           'priority'  => 'high'
         )
       ),
       array(
-        'cpt_name'       => 'exhibitions',
+        'cpt_name'       => 'Exhibitions',
         'cpt_supports'   => array( 'title', 'thumbnail', 'revisions', 'post-formats' ),
         'cpt_icon'       => 'dashicons-awards'
       )
