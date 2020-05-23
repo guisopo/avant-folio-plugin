b4ff08030bf3a46bc194e178301e101de2b11df1
guisopo <solerguillermo@hotmail.com>
2020-05-23 13:36:57 +0200

    Include functionality for select inputs

--- assets/avant-folio-inputs.js
+++ assets/avant-folio-inputs.js
@@ -1,7 +1,7 @@
 (function($) {
   
   class inputValueCopier {
-    constructor( inputID, input2ID) {
+    constructor(inputID, input2ID) {
       this.input = document.getElementById(inputID);
       this.input2 = document.getElementById(input2ID);
       
@@ -9,14 +9,26 @@
     }
   
     addEvents() {
-      document.addEventListener('keyup', () => this.copyTitle());
+      if (this.input2.type === 'text') {
+        this.input2.addEventListener('keyup', () => this.copyTitle());
+      } else {
+        this.input2.addEventListener('change', () => this.copyOption());
+      }
     }
 
     copyTitle() {
       this.input.value = this.input2.value;
     }
+
+    copyOption() {
+      this.input.value = this.input2.options[this.input2.selectedIndex].text;
+    }
   }
 
-  document.addEventListener("DOMContentLoaded", () => new inputValueCopier('workTitle', 'title'));
+  document.addEventListener("DOMContentLoaded", () => {
+    new inputValueCopier('work_title_key', 'title');
+    new inputValueCopier('work_type_key', 'work_type_select');
+    new inputValueCopier('date_completed_key', 'date_completed');
+  });
   
 })(jQuery);
