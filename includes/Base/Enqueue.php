88d7f9e7a1c3366fc6a64ec3d2f46b9a58b0e524
guisopo <solerguillermo@hotmail.com>
2020-08-18 15:14:52 +0200

    Improve exhibitions cpt and enqueue code

--- includes/Base/Enqueue.php
+++ includes/Base/Enqueue.php
@@ -13,10 +13,19 @@ class Enqueue extends BaseController
     add_action( 'admin_enqueue_scripts', array( $this, 'enqueue') );
   }
 
-  function enqueue() {
+  function enqueue($hook_suffix) {
     wp_enqueue_style( 'avant-folio-admin', $this->plugin_url . 'assets/avant-folio-admin.css' );
-    wp_enqueue_script( 'avant-folio-script', $this->plugin_url . 'assets/avant-folio-inputs.js' );
     wp_enqueue_media();
     wp_enqueue_script( 'avant-folio-gallery', $this->plugin_url . 'assets/avant-folio-gallery.js' );
+
+    $cpt = 'works';
+
+    if( in_array($hook_suffix, array('post.php', 'post-new.php') ) ){
+      $screen = get_current_screen();
+      if( is_object( $screen ) && $cpt == $screen->post_type ){
+        // Register, enqueue scripts and styles here
+        wp_enqueue_script( 'avant-folio-script-inputs', $this->plugin_url . 'assets/avant-folio-inputs.js' );
+      }
+  }
   }
 }
--- includes/Base/ExhibitionsCpt.php
+++ includes/Base/ExhibitionsCpt.php
@@ -6,32 +6,103 @@
 namespace Includes\Base;
 
 use Includes\Api\CustomPostType;
-use Includes\Api\BaseController;
+use Includes\Api\CustomField;
+use Includes\Api\ImageGallery;
 
-class ExhibitionsCpt extends BaseController {
+class ExhibitionsCpt {
 
   public $cpt = array();
 
   public function register() 
   {
-    $this->addCptData();
-    $this->createCpt();
+    $this->cpt_name = 'Exhibitions';
+    $this->gallery_title = 'Exhibitions Gallery';
+    $this->custom_field_id = 'exhibition-information';
+    $this->custom_field_title = 'Exhibition Details';
+    $this->meta_key = 'avant_folio_exhibition_info';
+    $this->gallery_meta_key = 'avant_folio_gallery';
+
+    $this->store_cpt_data();
+
+    isset( $this->cpt['arguments'] ) ? $this->create_cpt() : '';
   }
 
-  public function addCptData() 
+  public function store_cpt_data() 
+  {
+    $this
+      ->add_cpt_arguments()
+      ->add_cpt_custom_fields()
+      ->add_cpt_image_gallery();
+  }
+
+
+  public function add_cpt_arguments() 
   {
     $this->cpt['arguments'] = array(
-      'cpt_name'       => 'Exhibitions',
+      'cpt_name'       => $this->cpt_name,
       'cpt_supports'   => array( 'title', 'thumbnail', 'revisions', 'post-formats' ),
       'cpt_icon'       => 'dashicons-awards'
     );
+
+    return $this;
   }
 
-  public function createCpt() {
+  public function create_cpt() {
     $custom_post_type = new CustomPostType();
 
     $custom_post_type
       ->store_cpt( $this->cpt['arguments'] )
       ->register();
+
+    isset( $this->cpt['custom_fields'] ) ? $this->create_custom_fields() : '';
+    isset( $this->cpt['image_gallery'] ) ? $this->create_image_gallery() : '';
+  }
+
+  public function add_cpt_custom_fields() 
+  {
+    $this->cpt['custom_fields'] = array(
+      'id'       => $this->custom_field_id,
+      'title'    =>	esc_html__( $this->custom_field_title, 'string' ),
+      'screen'   => $this->cpt_name,
+      'meta-key' => $this->meta_key
+    );
+
+    return $this;
+  }
+
+  public function create_custom_fields() 
+  {
+    $custom_fields = new CustomField();
+
+		$custom_fields
+			->set_metabox( $this->cpt['custom_fields'] )
+			->register();
+  }
+
+  public function add_cpt_image_gallery() 
+  {
+    $this->cpt['image_gallery'] = array(
+      array (
+        'id'       => $this->gallery_title,
+        'title'    =>	esc_html__( $this->gallery_title, 'string' ),
+        'screen'   => $this->cpt_name,
+        'context'	 => 'advanced',
+        'priority' => 'high',
+        'meta-key' => $this->gallery_meta_key
+      )
+    );
+
+    return $this;
+  }
+
+  public function create_image_gallery() 
+  {
+    foreach ($this->cpt['image_gallery'] as $gallery_arguments) {
+      $gallery = new ImageGallery();
+      
+      $gallery
+        ->set_gallery( $gallery_arguments )
+        ->register();
+    }
   }
 }
